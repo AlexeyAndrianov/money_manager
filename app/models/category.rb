3dd5f4fcba93a56c529f6855acc2fee31bff8f38
Alexey <alexandrianovmail@gmail.com>
2017-04-05 18:42:13 +0300

    Tests (#20)
    * moar tests
    
    * tests latest version
    
    * planned amount recalculation bug. after deleting category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :user
-  has_many :plans
+  has_many :plans, dependent: :destroy
 
 	validates :name, presence: true
   validates :name, uniqueness: { scope: :user }
